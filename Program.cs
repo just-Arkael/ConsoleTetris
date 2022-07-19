d9b9f16580255967a0544a5fefa5300ad4a9e722
Arkael <boltthedog27@gmail.com>
2022-07-19 14:11:36 +0300

    Added randomized figures

--- Program.cs
+++ Program.cs
@@ -8,6 +8,118 @@ namespace Tetris
 {
     internal class Program
     {
+
+        static void NextFigure(ref int[] figure1, ref int[] figure2, ref int[] figure3, ref int[] figure4, ref byte newFigure, ref Random rand)
+        {
+
+            switch (newFigure)
+            {
+
+                case 0:
+                    figure1[0] = 3;
+                    figure1[1] = 0;
+                    figure2[0] = 4;
+                    figure2[1] = 0;
+                    figure3[0] = 5;
+                    figure3[1] = 0;
+                    figure4[0] = 6;
+                    figure4[1] = 0;
+                    break;
+                case 1:
+                    figure1[0] = 4;
+                    figure1[1] = 0;
+                    figure2[0] = 4;
+                    figure2[1] = 1;
+                    figure3[0] = 5;
+                    figure3[1] = 1;
+                    figure4[0] = 6;
+                    figure4[1] = 1;
+                    break;
+                case 2:
+                    figure1[0] = 6;
+                    figure1[1] = 0;
+                    figure2[0] = 4;
+                    figure2[1] = 1;
+                    figure3[0] = 5;
+                    figure3[1] = 1;
+                    figure4[0] = 6;
+                    figure4[1] = 1;
+                    break;
+                case 3:
+                    figure1[0] = 4;
+                    figure1[1] = 0;
+                    figure2[0] = 4;
+                    figure2[1] = 1;
+                    figure3[0] = 5;
+                    figure3[1] = 0;
+                    figure4[0] = 5;
+                    figure4[1] = 1;
+                    break;
+                case 4:
+                    figure1[0] = 5;
+                    figure1[1] = 0;
+                    figure2[0] = 6;
+                    figure2[1] = 0;
+                    figure3[0] = 4;
+                    figure3[1] = 1;
+                    figure4[0] = 5;
+                    figure4[1] = 1;
+                    break;
+                case 5:
+                    figure1[0] = 5;
+                    figure1[1] = 0;
+                    figure2[0] = 4;
+                    figure2[1] = 1;
+                    figure3[0] = 5;
+                    figure3[1] = 1;
+                    figure4[0] = 6;
+                    figure4[1] = 1;
+                    break;
+                case 6:
+                    figure1[0] = 4;
+                    figure1[1] = 0;
+                    figure2[0] = 5;
+                    figure2[1] = 0;
+                    figure3[0] = 5;
+                    figure3[1] = 1;
+                    figure4[0] = 6;
+                    figure4[1] = 1;
+                    break;
+                case 7:
+                    figure1[0] = 5;
+                    figure1[1] = 0;
+                    figure2[0] = 6;
+                    figure2[1] = 0;
+                    figure3[0] = 4;
+                    figure3[1] = 0;
+                    figure4[0] = 5;
+                    figure4[1] = 1;
+                    break;
+                case 8:
+                    figure1[0] = 4;
+                    figure1[1] = 0;
+                    figure2[0] = 5;
+                    figure2[1] = 0;
+                    figure3[0] = 6;
+                    figure3[1] = 0;
+                    figure4[0] = 4;
+                    figure4[1] = 1;
+                    break;
+                case 9:
+                    figure1[0] = 4;
+                    figure1[1] = 0;
+                    figure2[0] = 5;
+                    figure2[1] = 0;
+                    figure3[0] = 6;
+                    figure3[1] = 0;
+                    figure4[0] = 6;
+                    figure4[1] = 1;
+                    break;
+
+            }
+            newFigure = (byte)rand.Next(9);
+
+        }
         static void MoveFigureLeft(bool[,] board, int[] figure1, int[] figure2, int[] figure3, int[] figure4)
         {
             if (figure1[0] > 0 && board[figure1[0], figure1[1]] == false && figure2[0] > 0 && board[figure2[0], figure2[1]] == false && figure3[0] > 0 && board[figure3[0], figure3[1]] == false && figure4[0] > 0 && board[figure4[0], figure4[1]] == false)
@@ -100,7 +212,9 @@ namespace Tetris
             int[] figure2 = { 4, 1 };
             int[] figure3 = { 5, 1 };
             int[] figure4 = { 6, 1 };
-            
+            byte newFigure = (byte)rand.Next(9);
+
+
         Reset:
             
 
@@ -108,14 +222,7 @@ namespace Tetris
             DrawBoard(board, figure1, figure2, figure3, figure4);
             if (MoveFigureDown(ref board, figure1, figure2, figure3, figure4) == false)
             {
-                figure1[0] = 5;
-                figure1[1] = 0;
-                figure2[0] = 4;
-                figure2[1] = 1;
-                figure3[0] = 5;
-                figure3[1] = 1;
-                figure4[0] = 6;
-                figure4[1] = 1;
+                NextFigure(ref figure1, ref figure2, ref figure3, ref figure4, ref newFigure, ref rand);
             }
 
             switch (Console.ReadKey().Key)
